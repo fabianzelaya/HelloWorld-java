a19d387ee6424da68d70a746acf30b87edbe2e81
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-01-29 18:43:10 -0800

    Init

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,6 @@
-
 // My first Java program
-import java.util.*;
 
-public class Main {
+public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
