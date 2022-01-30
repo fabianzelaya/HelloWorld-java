2717cddaaf5b096b8c3435e149210d8edd3090ad
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-01-29 18:45:39 -0800

    Init

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,7 @@
 // My first Java program
 
 public class helloWorld {
+    // Output "Hello World" to the counsole
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
