14a4879a95f2fa9ecb1d7da5215d21335f6c1a53
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-01-31 01:40:55 -0800

    Init

--- helloWorld.java
+++ helloWorld.java
@@ -2,8 +2,16 @@
 
 public class helloWorld {
     // Output "Hello World" to the counsole
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Method
         System.out.println("Hello, World!");
     }
 }
 // FZ
+
+//Double slash create comments
+//Class - Contains everything - has members
+//Methods - Do something
+//Properties - Store something
+//Access modifiers (public or private) - Who can use?
+//Static - No instance of class is needed
+//Object - Instance of class
