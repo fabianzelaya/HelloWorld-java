7db1bbaa41c12958d5f900742ad65dc9c2a4122b
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-01-31 10:50:32 -0800

    Init

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,7 +3,8 @@
 public class helloWorld {
     // Output "Hello, World!" to the counsole
     public static void main(String[] args) { // Method
-        System.out.println("Hello " + args[0]); //, World!
+        System.out.println("Hello " + args[0]);
+        //System.out.println("Hello, World!");
     }
 }
 // FZ
